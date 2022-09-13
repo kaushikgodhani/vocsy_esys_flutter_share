7ca39acde0dade4c6286f259c55738cc68c1f9e7
Brett Morgan <brett.morgan@gmail.com>
2022-09-13 04:34:50 +0000

    Fixing up linting warnings

--- lib/vocsy_esys_flutter_share.dart
+++ lib/vocsy_esys_flutter_share.dart
@@ -5,15 +5,15 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 class Share {
-  static const MethodChannel _channel = const MethodChannel(
+  static const MethodChannel _channel = MethodChannel(
       'channel:github.com/orgs/esysberlin/esys-flutter-share');
 
   /// Sends a text to other apps.
   static void text(String title, String text, String mimeType) {
     Map argsMap = <String, String>{
-      'title': '$title',
-      'text': '$text',
-      'mimeType': '$mimeType'
+      'title': title,
+      'text': text,
+      'mimeType': mimeType
     };
     _channel.invokeMethod('text', argsMap);
   }
@@ -22,14 +22,14 @@ class Share {
   static Future<void> file(
       String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
     Map argsMap = <String, String>{
-      'title': '$title',
-      'name': '$name',
-      'mimeType': '$mimeType',
-      'text': '$text'
+      'title': title,
+      'name': name,
+      'mimeType': mimeType,
+      'text': text
     };
 
     final tempDir = await getTemporaryDirectory();
-    final file = await new File('${tempDir.path}/$name').create();
+    final file = await File('${tempDir.path}/$name').create();
     await file.writeAsBytes(bytes);
 
     _channel.invokeMethod('file', argsMap);
@@ -39,16 +39,16 @@ class Share {
   static Future<void> files(
       String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
     Map argsMap = <String, dynamic>{
-      'title': '$title',
+      'title': title,
       'names': files.entries.toList().map((x) => x.key).toList(),
       'mimeType': mimeType,
-      'text': '$text'
+      'text': text
     };
 
     final tempDir = await getTemporaryDirectory();
 
     for (var entry in files.entries) {
-      final file = await new File('${tempDir.path}/${entry.key}').create();
+      final file = await File('${tempDir.path}/${entry.key}').create();
       await file.writeAsBytes(entry.value);
     }
 
